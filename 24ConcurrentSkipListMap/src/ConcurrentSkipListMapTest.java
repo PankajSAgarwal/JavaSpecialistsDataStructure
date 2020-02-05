86c0e789ce0b52fbd2760fa3d57f7f79abc4d785
Agarwal, Pankaj <2005pank@gmail.com>
2020-02-05 08:08:03 +0530

    ConcurrentSkipListMap

--- 24ConcurrentSkipListMap/src/ConcurrentSkipListMapTest.java
+++ 24ConcurrentSkipListMap/src/ConcurrentSkipListMapTest.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.IntStream;
 
 public class ConcurrentSkipListMapTest {
+
+    public static void main(String[] args) {
+        // Gives sorted order with thread safety
+       Map<Integer,Integer> squares = new ConcurrentSkipListMap<>();
+
+        // TreeMap Doesn't give full 10,000 numbers but 9768 numbers
+       // Map<Integer,Integer> squares = new TreeMap<>();
+        // HashMap doesnt give full 10,000 numbers but reduced numbers
+        //Map<Integer,Integer> squares = new HashMap<>();
+        // Concurrent Hashmap gives full range of numbers but not necessarily in sorted order
+        //Map<Integer,Integer> squares = new ConcurrentHashMap<>();
+
+        IntStream.range(0,10_000).parallel().forEach(
+                i->squares.put(i,i*i)
+        );
+
+        System.out.println("squares = " + squares);
+        System.out.println("squares.size() = " + squares.size());
+        
+
+    }
 }
