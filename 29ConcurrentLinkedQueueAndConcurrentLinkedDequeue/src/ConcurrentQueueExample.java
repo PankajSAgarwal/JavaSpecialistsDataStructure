1301441b6a14252aeef212b0e7acd44afe3a5a03
Agarwal, Pankaj <2005pank@gmail.com>
2020-02-06 08:43:43 +0530

    ConcurrentLinkedQueue and ConcurrentLinkedDeque Java Queue DataStructures

---
+++ 29ConcurrentLinkedQueueAndConcurrentLinkedDequeue/src/ConcurrentQueueExample.java
@@ -0,0 +1,33 @@
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.LongAccumulator;
+import java.util.stream.IntStream;
+
+public class ConcurrentQueueExample {
+    public static void main(String[] args) {
+        Queue<Integer> clq = new ConcurrentLinkedQueue<>();
+        System.out.println(clq.offer(1));
+        System.out.println(clq.add(2));
+        System.out.println(clq.poll());
+        System.out.println(clq.poll());
+        System.out.println(clq.poll());
+
+        System.out.println(clq.size()); // O(n)
+
+        LongAccumulator maxSize = new LongAccumulator(Long::max,0);
+
+        IntStream.range(0,100_000_000)
+                .parallel()
+                .forEach(i->{
+                    clq.offer(i);
+                    maxSize.accumulate(clq.size());
+                    clq.poll();
+                });
+        System.out.println("maxSize = " + maxSize);
+
+        // lifo or fifo or some hybrid
+        Queue<Integer> cld = new ConcurrentLinkedDeque<>();
+        
+    }
+}
