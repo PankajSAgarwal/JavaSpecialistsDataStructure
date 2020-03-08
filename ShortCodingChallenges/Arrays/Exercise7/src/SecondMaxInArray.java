85aed31eb40e7076de8dca52e457e0dce172e932
Agarwal, Pankaj <2005pank@gmail.com>
2020-03-08 19:01:42 +0530

    Array Challenges

--- ShortCodingChallenges/Arrays/Exercise7/src/SecondMaxInArray.java
+++ ShortCodingChallenges/Arrays/Exercise7/src/SecondMaxInArray.java
@@ -6,12 +6,20 @@ public class SecondMaxInArray {
 
         System.out.println("Array: " + arrayToString(arr));
 
-        int maxSecond = findSecondMaximum(arr);
-        System.out.println("Second maximum: " + maxSecond);
+        int maxSecondTwiceArrayTraversal = findSecondMaximumByTwiceArrayTraversal(arr);
+        System.out.println("### Solution 1 #####");
+        System.out.println("Second maximum by traversing array twice: " + maxSecondTwiceArrayTraversal);
+
+        System.out.println("### Solution 2 #####");
+        int maxSecondOnceArrayTraversal = findSecondMaximumByOnceArrayTraversal(arr);
+        System.out.println("Second maximum by traversing array once: " + maxSecondOnceArrayTraversal);
+
+
 
     }
 
-    public static int findSecondMaximum(int[] arr){
+    // Solution 1 - by Traversing array Twice
+    public static int findSecondMaximumByTwiceArrayTraversal(int[] arr){
 
         int max = Integer.MIN_VALUE;
         int maxSecond = Integer.MIN_VALUE;
@@ -34,6 +42,31 @@ public class SecondMaxInArray {
             if(arr[i] > maxSecond && arr[i] != max){
                 maxSecond = arr[i];
             }
+        } // end of for loop
+
+        return maxSecond;
+    }
+
+    // Solution 2 - by Traversing array Once
+    public static int findSecondMaximumByOnceArrayTraversal(int[] arr){
+
+        int max = Integer.MIN_VALUE;
+        int maxSecond = Integer.MIN_VALUE;
+
+        // Find the maximum value in the array by comparing each index with max
+        // If an element is greater than max then update max to be equal to it
+        for (int i = 0; i < arr.length ; i++) {
+
+            if(arr[i] > max){
+
+                maxSecond = max ;
+                max= arr[i];
+
+            }else if(arr[i] > maxSecond && arr[i] != max ){
+
+                maxSecond = arr[i];
+            }
+
         } // end of for loop
 
         return maxSecond;
---
+++ ShortCodingChallenges/Arrays/Exercise8/README.md
@@ -0,0 +1,18 @@
+#Problem Statement -  Right Rotate the Array by One Index
+Given an array, can you rotate its elements once from right to left?
+
+## function prototype
+void rotateArray(int[] arr)
+
+## Sample input
+
+arr = {1, 2, 3, 4, 5}
+
+
+## Sample output
+
+arr = {5, 1, 2, 3, 4}
+
+## Time Complexity
+
+Since the entire array is iterated over once, the time complexity of this solution is O(n).
